7f3bf523c9fb85ea6c6047a27a0836d69daa95f3
julieelie <julieelie@users.noreply.github.com>
2018-11-19 14:08:04 -0800

    Update score_place_test.m
    Preventing mistyping and quiting the function

--- score_place_test.m
+++ score_place_test.m
@@ -1,11 +1,12 @@
 % Make new file for the experiment 
-function [] = score_place_test(Name,Date, Exptype, Path)
-Filename = fullfile(Path,sprintf('%d_%s_%s.txt', Date, Name, Exptype));
-FileID = fopen(Filename);
-fprintf(FileID, 'position\telapsedtime\n' );
+function [] = score_place_test(Name, Exptype, Path)
+Filename = fullfile(Path,sprintf('%s_%s_%s.txt', datestr(now, 'yymmdd_HHMM'), Name, Exptype));
+FileID = fopen(Filename,'wt');
+fprintf(FileID, 'position\telapsedtime(s)\n' );
+Test_duration = 15*60;
 Tstart = tic;
-while 1
-    Prompt = input('Boundary crossed','s');
+while toc(Tstart)<Test_duration
+    Prompt = input('Moving to which area? (Middle:m; Left:l; Right:r; any other key:stop)\n','s');
     if strcmp(Prompt, 'r')
         fprintf(FileID,'right\t%f\n',toc(Tstart));
     elseif strcmp(Prompt, 'l')
@@ -13,8 +14,14 @@ while 1
     elseif strcmp(Prompt, 'm')
         fprintf(FileID,'middle\t%f\n',toc(Tstart));
     else
-        break
+        Prompt2 = input('Do you really want to stop? (Yes:y; No:n)\n','s');
+        if strcmp(Prompt2, 'n')
+        elseif strcmp(Prompt2, 'y')
+            fprintf(FileID,'stop\t%f\n',toc(Tstart));
+            break
+        end
     end
 end
 fclose(FileID);
+fprintf(1,'*** The test is over! ***\n*** Run for %.2f min ***\n', toc(Tstart)/60);
 end
