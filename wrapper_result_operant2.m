ce695d70e92d9b7ce0b57f042b6a3013ed30e29c
julieelie <julie.elie@gmail.com>
2019-11-30 12:49:04 -0800

    Update wrapper_result_operant2.m
    adding a screening of data already run

--- wrapper_result_operant2.m
+++ wrapper_result_operant2.m
@@ -4,15 +4,34 @@ OutputDataPath = 'Z:\users\tobias\vocOperant\Results';
 BaseDir = 'Z:\users\tobias\vocOperant';
 BoxOfInterest = [3 4 6 8];
 ExpLog = fullfile(OutputDataPath, 'VocOperantLogWhoCalls.txt');
-Fid = fopen(ExpLog, 'a');
+
 if ~exist(ExpLog, 'file')
+    Fid = fopen(ExpLog, 'a');
     fprintf(Fid, 'Subject\tDate\tTime\tType\tDuration(s)\tLoggerData\n');
+    DoneList = [];
+else
+    Fid = fopen(ExpLog, 'r');
+    Header = textscan(Fid,'%s\t%s\t%s\t%s\t%s\t%s\n');
+    DoneList = textscan(Fid,'%s\t%s\t%s\t%s\t%.1f\t%d');
+    fclose(Fid);
+    Fid = fopen(ExpLog, 'a');
+    
 end
 
 for bb=1:length(BoxOfInterest)
     ParamFilesDir = dir(fullfile(BaseDir,sprintf('box%d',BoxOfInterest(bb)),'bataudio','*_VocTrigger_param.txt'));
-    for ff=3:length(ParamFilesDir)
+    for ff=1:length(ParamFilesDir)
         filepath = fullfile(ParamFilesDir(ff).folder, ParamFilesDir(ff).name);
+        fprintf(1,'\n\n\nBox %d (%d/%d), file %d/%d:\n%s\n',BoxOfInterest(bb),bb,length(BoxOfInterest),ff,length(ParamFilesDir),filepath)
+        % Check that the file was not already treated
+        BatsID = ParamFilesDir(ff).name(1:4);
+        Date = ParamFilesDir(ff).name(6:11);
+        Time = ParamFilesDir(ff).name(13:16);
+        Done = sum(contains(DoneList{1},BatsID) .* contains(DoneList{2},Date) .* contains(DoneList{3},Time));
+        if Done
+            fprintf(1, '   -> Data already processed\n')
+            continue
+        end
         % check that the experiment has data!~
         fid = fopen(filepath);
         data = textscan(fid,'%s','Delimiter', '\t');
