fb98c325f74d629b25c3e87e7fe50e90af75754c
julieelie <julie.elie@gmail.com>
2020-02-27 19:14:47 -0800

    adding extraction of reward times to voc_localize_operant.m

--- result_operant_bat.m
+++ result_operant_bat.m
@@ -5,6 +5,7 @@ addpath(genpath('/Users/elie/Documents/CODE/SoundAnalysisBats'))
 TranscExtract = 1; % set to 1 to extract logger data and transceiver time
 ForceExtract = 0; % set to 1 to redo the extraction of loggers otherwise the calculations will use the previous extraction data
 ForceAllign = 0; % In case the TTL pulses allignment was already done but you want to do it again, set to 1
+ForceReExt = 1; % In case the extraction of reward times was not done and you want to do it
 ForceVocExt1 = 0; % In case the extraction of vocalizations that triggered rewarding system was already done but you want to do it again set to 1
 ForceVocExt2 = 0; % In case the extraction of vocalizations that triggered rewarding system was already done but you want to do it again set to 1
 ForceWhoID = 0; % In case the identification of bats was already done but you want to re-do it again
@@ -483,6 +484,10 @@ if TranscExtract
         fprintf(1,'*** ALREADY DONE: Localizing and extracting vocalizations that triggered the sound detection ***\n');
     end
     
+    if ForceReExt
+        fprintf(1,'*** Localizing and extracting reward times ***\n');
+        re_localize_operant(AudioDataPath, DataFile(1:4),Date, ExpStartTime)
+    end
     %% Identify the same vocalizations on the piezos and save sound extracts, onset and offset times
     fprintf(' LOCALIZING VOCALIZATIONS ON PIEZO RECORDINGS\n')
     LogVoc_dir = dir(fullfile(Logger_dir, sprintf('%s_%s_VocExtractData.mat', Date, ExpStartTime)));
