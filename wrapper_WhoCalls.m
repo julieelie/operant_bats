fa111e22c19bf2dcc08ac7d9b15b28b20c9a2424
Julie E Elie <julie.elie@gmail.com>
2020-11-03 18:37:41 -0800

    removed commented line

--- wrapper_WhoCalls.m
+++ wrapper_WhoCalls.m
@@ -7,10 +7,10 @@ BaseDir = 'Z:\users\tobias\vocOperant';
 % PriorityDates1 = [180926 190826];
 % PriorityPairs2 = {'ClEn' 'EnCl' 'CoEd' 'EdCo' 'EzEl' 'ElEz' 'HaHo' 'HoHa'};
 % PriorityDates2 = [161122 190603];
-ExpLog = fullfile(OutputDataPath, 'VocOperantLogWhoCalls.txt');
-WhoLog = fullfile(OutputDataPath, 'VocOperantLogWhoCallsDone.txt');
-CheckAllignmentLog = fullfile(OutputDataPath, 'VocOperantLogCheckAllignement.txt');
-TrashEcholocationCalls = fullfile(OutputDataPath, 'VocOperantLogEcholocation.txt');
+ExpLog = fullfile(OutputDataPath, 'VocOperantLogWhoCallsNew.txt');
+WhoLog = fullfile(OutputDataPath, 'VocOperantLogWhoCallsDoneNew.txt');
+CheckAllignmentLog = fullfile(OutputDataPath, 'VocOperantLogCheckAllignementNew.txt');
+TrashEcholocationCalls = fullfile(OutputDataPath, 'VocOperantLogEcholocationNew.txt');
 
 FidExp = fopen(ExpLog, 'r');
 Header = textscan(FidExp,'%s\t%s\t%s\t%s\t%s\t%s\n');
@@ -32,12 +32,12 @@ end
 
 if ~exist(CheckAllignmentLog, 'file')
     FidCheck = fopen(CheckAllignmentLog, 'a');
-    fprintf(FidCheck, 'Subject\tDate\tTime\tType\tDuration(s)\n');
+    fprintf(FidCheck, 'Subject\tDate\tTime\tAlignement\n');
     CrapList = [];
 else
     FidCheck = fopen(CheckAllignmentLog, 'r');
-    Header2 = textscan(FidCheck,'%s\t%s\t%s\t%s\t%s',1);
-    CrapList = textscan(FidCheck,'%s\t%s\t%s\t%s\t%.1f');
+    Header2 = textscan(FidCheck,'%s\t%s\t%s\t%s\n',1);
+    CrapList = textscan(FidCheck,'%s\t%s\t%s\t%s\n');
     fclose(FidCheck);
     FidCheck = fopen(CheckAllignmentLog, 'a');
     
@@ -64,19 +64,20 @@ box_line = find(contains(data{1}, 'box'));
 
 for ff=1:length(name_line)
     f_name = data{1}{name_line(ff)};
-    ParamFilesDir = dir(fullfile(BaseDir, 'Exp_Stats', f_name));
-    filepath = fullfile(BaseDir, 'Exp_Stats', f_name);
+    BatsID = f_name(1:4);
+    Date = f_name(6:11);
+    Time = f_name(13:16);
+    boxID = data{1}{box_line(ff)};
+    ParamFilesDir = dir(fullfile(BaseDir, boxID, 'bataudio', f_name));
+    filepath = fullfile(BaseDir, boxID, 'bataudio', f_name);
     fprintf(1,'file %d/%d:\n%s\n', ff, length(name_line),filepath)
     % Check if that file is on the
     % priority list! then on the ToDo list (list of data that has
     % already been extracted by wrapper_result_operant2), if it's been
     % already done or labbeled as crappy.
-    BatsID = f_name(1:4);
-    Date = f_name(6:11);
+    
 %     if (sum(contains(PriorityPairs1, BatsID)) && (str2double(Date)>PriorityDates1(1)) && (str2double(Date)<PriorityDates1(2))) || (sum(contains(PriorityPairs2, BatsID)) && (str2double(Date)>PriorityDates2(1)) && (str2double(Date)<PriorityDates2(2)))
 %         if ~((strcmp(BatsID, 'TeTa') || strcmp(BatsID, 'TaTe')) && str2double(Date)<190618) % TeTa Bats were doing echolocation calls before June 18th 2019
-            Time = f_name(13:16);
-            boxID = data{1}{box_line(ff)};
             ToDo = find(contains(ToDoList{1},BatsID) .* contains(ToDoList{2},Date) .* contains(ToDoList{3},Time).*logical(ToDoList{6}));
             Done = find(contains(DoneList{1},BatsID) .* contains(DoneList{2},Date) .* contains(DoneList{3},Time).*logical(DoneList{6}));
             Crap = find(contains(CrapList{1},BatsID) .* contains(CrapList{2},Date) .* contains(CrapList{3},Time));
@@ -104,7 +105,7 @@ for ff=1:length(name_line)
                         end
 
                 fprintf(1,'*** Check the allignement of the TTL pulses ***\n')
-                AllignmentPath = fullfile(BaseDir,sprintf('box%d',BoxOfInterest(bb)),'bataudio',sprintf('%s_%s_CD_correction_audio_piezo.fig', Date, Time));
+                AllignmentPath = fullfile(BaseDir,boxID,'bataudio',sprintf('%s_%s_CD_correction_audio_piezo.fig', Date, Time));
                 FigAP = open(AllignmentPath);
                 %                 fprintf(1, 'Go in %s\n',fullfile(BaseDir,sprintf('box%d',BoxOfInterest(bb)),'bataudio'))
                 %                 fprintf(1,'Search for %s_%s_CD_correction_audio_piezo\n', Date, Time)
@@ -112,18 +113,19 @@ for ff=1:length(name_line)
                 fprintf('\n')
                 close(FigAP)
 
+                % Save files to CheckAllignementLog if misaligned
                 if sum(Check)~=length(Check)
-                    Ind_ = strfind(ParamFilesDir(ff).name, '_param');
-                    fprintf(FidCheck, '%s\t%s\t%s\t%s\t%.1f\n',ParamFilesDir(ff).name(1:4),ParamFilesDir(ff).name(6:11),ParamFilesDir(ff).name(13:16),ParamFilesDir(ff).name(18:(Ind_-1)),Temp);
+                    Ind_ = strfind(f_name, '_param');
+                    fprintf(FidCheck, '%s\t%s\t%s\t%s\n',f_name(1:4),f_name(6:11),f_name(13:16),Check(length(LoggersDir)+1));
                 else
                     EchoYes = input('Did you identify that file as at least having the first 10 sequences being echolocation calls? (yes ->1, No -> 0): ');
                     if  ~EchoYes
                         WhoDataYN = result_operant_bat2_who(filepath);
-                        Ind_ = strfind(ParamFilesDir(ff).name, '_param');
-                        fprintf(FidWho, '%s\t%s\t%s\t%s\t%.1f\t%d\n',ParamFilesDir(ff).name(1:4),ParamFilesDir(ff).name(6:11),ParamFilesDir(ff).name(13:16),ParamFilesDir(ff).name(18:(Ind_-1)),Temp,WhoDataYN);
+                        Ind_ = strfind(f_name, '_param');
+                        fprintf(FidWho, '%s\t%s\t%s\t%s%d\n',f_name(1:4),f_name(6:11),f_name(13:16),Check(length(LoggersDir)+1),WhoDataYN);
                     else
-                        Ind_ = strfind(ParamFilesDir(ff).name, '_param');
-                        fprintf(FidEcho, '%s\t%s\t%s\t%s\t%.1f\n',ParamFilesDir(ff).name(1:4),ParamFilesDir(ff).name(6:11),ParamFilesDir(ff).name(13:16),ParamFilesDir(ff).name(18:(Ind_-1)),Temp);
+                        Ind_ = strfind(f_name, '_param');
+                        fprintf(FidEcho, '%s\t%s\t%s\t%s\t%.1f\n',f_name(1:4),f_name(6:11),f_name(13:16),f_name(18:(Ind_-1)),Temp);
                     end
                 end
                     end
