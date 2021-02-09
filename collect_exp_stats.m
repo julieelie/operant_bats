2c609cc6aa8d1b9c1e4df3f88c5a356d398b65c1
Julie Elie <julie.elie@gmail.com>
2021-02-09 10:53:47 -0800

    adding more features to stats collection and restricting analysis to files with logger data

--- collect_exp_stats.m
+++ collect_exp_stats.m
@@ -1,14 +1,26 @@
-% This script gather all the VocTrigger experiments longer than 10min
-% NOTE: We are focusing on VocTrigger experiments that are longer than 5
+% This script gather all the VocTrigger experiments longer than 10min with
+% aminimum of 5 calls
+% NOTE: We are focusing on VocTrigger experiments that are longer than 10
 % min.
 MinVoc = 5;
+MinDur = 10;%(in min)
 OutputDataPath = 'Z:\users\tobias\vocOperant\Exp_Stats';
 BaseDir = 'Z:\users\tobias\vocOperant';
 BoxOfInterest = [1 2 3 4 5 6 7 8];
 ExpLog = fullfile(OutputDataPath, 'VocOperantData.txt');
+AllLog = fullfile(OutputDataPath, 'VocOperantAllData.txt');
+diary(fullfile(OutputDataPath, 'StatsDiary.txt'));
+AllExpCount = 0;
+ExpMissMicDataCount = 0;
+ExpTooShortCount = 0;
+ExpTooFewCallsCount = 0;
+ExpMissLogDataCount = 0;
+
 
 Fid = fopen(ExpLog, 'w');
+FidAll = fopen(AllLog, 'w');
 fprintf(Fid, 'FileName\tBoxNum\tHigh-Pass(Hz)\tLow-Pass(Hz)\tNumVocs\tDuration(min)\n');
+fprintf(FidAll, 'FileName\tBoxNum\tHigh-Pass(Hz)\tLow-Pass(Hz)\tNumVocs\tDuration(min)\tMicData\tLoggerData\n');
 
 
 for bb=1:length(BoxOfInterest) % for each box
@@ -17,10 +29,12 @@ for bb=1:length(BoxOfInterest) % for each box
     for ff=1:length(ParamFilesDir)
         
         Filepath = fullfile(ParamFilesDir(ff).folder, ParamFilesDir(ff).name);
-        fprintf(1,'\n\n\nBox %d (%d/%d), file %d/%d:\n%s\n',BoxOfInterest(bb),bb,length(BoxOfInterest),ff,length(ParamFilesDir),Filepath)
+        BoxID = BoxOfInterest(bb);
+        fprintf(1,'\n\n\nBox %d (%d/%d), file %d/%d:\n%s\n',BoxID,bb,length(BoxOfInterest),ff,length(ParamFilesDir),Filepath)
         
         
         % check that the experiment has data!
+        MicFiles = dir(fullfile(ParamFilesDir(ff).folder, [ParamFilesDir(ff).name(1:25) 'mic1*']));
         fid = fopen(Filepath);
         data = textscan(fid,'%s','Delimiter', '\t');
         fclose(fid);
@@ -32,16 +46,17 @@ for bb=1:length(BoxOfInterest) % for each box
             % find the data into that line
             Temp = round(str2double(data{1}{IndexLine}((IndexChar + 6):(IndexChar2-2)))/60);% duration in min
         else %No stop line, estimate the duration of the experiment by looking at the number of microphone files
-            MicFiles = dir(fullfile(ParamFilesDir(ff).folder, [ParamFilesDir(ff).name(1:25) 'mic1*']));
             Temp = (length(MicFiles)-1)*10;
         end
-        if Temp<10
-            fprintf(1, '   -> Data too short\n')
-            continue
-        end
+        MicData = isempty(MicFiles);
+        
+        % Check that the experiment has logger data
+        Logger_dir = fullfile(AudioDataPath(1:(strfind(ParamFilesDir(ff).folder, 'bataudio')-1)), 'piezo',ParamFilesDir(ff).name(6:11),'audiologgers');
+        All_loggers_dir = dir(fullfile(Logger_dir, '*ogger*'));
+        LogData = isempty(Logger_dir);
         
         % Find corresponding event file and get number of vocalizations
-        DataFileStruc = dir(fullfile(BaseDir,sprintf('box%d',BoxOfInterest(bb)), 'bataudio', [ParamFilesDir(ff).name(1:16), '*_VocTrigger_events.txt']));
+        DataFileStruc = dir(fullfile(BaseDir,sprintf('box%d',BoxID), 'bataudio', [ParamFilesDir(ff).name(1:16), '*_VocTrigger_events.txt']));
         numVocs = -1; % set numVocs to < 0 if no data
         if ~isempty(DataFileStruc) > 0
             Fid_Data = fopen(fullfile(DataFileStruc.folder,DataFileStruc.name));
@@ -56,27 +71,55 @@ for bb=1:length(BoxOfInterest) % for each box
         % FIND the low pass and high pass filters applied
         IndexLineHigh = find(contains(data{1}, 'high-pass'));
         IndexLineLow = find(contains(data{1}, 'low-pass'));
-        if ~isempty(IndexLineLow) && ~isempty(IndexLineHigh) && numVocs >= MinVoc
-            IndexCharHigh = strfind(data{1}{IndexLineHigh},'threshold: ');
-            IndexChar2High = strfind(data{1}{IndexLineHigh},'Hz');
-            % find the data (high threshold) in that line
-            high = str2double(data{1}{IndexLineHigh}((IndexCharHigh + 11):(IndexChar2High-2)));
-            
+        if ~isempty(IndexLineLow)
             IndexChar = strfind(data{1}{IndexLineLow},'threshold: ');
             IndexChar2 = strfind(data{1}{IndexLineLow},'Hz');
             % find the data (low threshold) in that line
             low = str2double(data{1}{IndexLineLow}((IndexChar + 11):(IndexChar2-2)));
-            
-            boxID = BoxOfInterest(bb);
+        else
+            low = Nan;
+        end
+        
+        if ~isempty(IndexLineHigh)
+            IndexCharHigh = strfind(data{1}{IndexLineHigh},'threshold: ');
+            IndexChar2High = strfind(data{1}{IndexLineHigh},'Hz');
+            % find the data (high threshold) in that line
+            high = str2double(data{1}{IndexLineHigh}((IndexCharHigh + 11):(IndexChar2High-2)));
+        else
+            high = Nan;
+        end
+        
+        if (numVocs >= MinVoc) && (Temp>MinDur) && ~MicData && ~LogData
             try
                 % prints batIDs, date, time, low threshold, high threshold, and number of vocalizations
-                fprintf(Fid, '%s\t%s%d\t%f\t%f\t%d\t%d\n',ParamFilesDir(ff).name, 'box', boxID, high, low, numVocs, Temp);
+                fprintf(Fid, '%s\t%s%d\t%f\t%f\t%d\t%d\n',ParamFilesDir(ff).name, 'box', BoxID, high, low, numVocs, Temp);
+                fprintf(FidAll, '%s\t%s%d\t%f\t%f\t%d\t%d\n',ParamFilesDir(ff).name, 'box', BoxID, high, low, numVocs, Temp, ~MicData, ~LogData);
             catch
                 keyboard
             end
+        else
+            fprintf(FidAll, '%s\t%s%d\t%f\t%f\t%d\t%d\n',ParamFilesDir(ff).name, 'box', BoxID, high, low, numVocs, Temp,  ~MicData, ~LogData);
         end
+        
+        AllExpCount = AllExpCount +1;
+        ExpMissMicDataCount = ExpMissMicDataCount + MicData;
+        if ~MicData % There are mic data
+            ExpTooShortCount = ExpTooShortCount + (Temp<=MinDur) ; % count if too short
+            if Temp>MinDur % There are mic data and long enough
+                ExpTooFewCallsCount = ExpTooFewCallsCount + (numVocs < MinVoc); % count if note enough calls
+                if numVocs >= MinVoc% There are mic data and long enough and enough calls
+                    ExpMissLogDataCount = ExpMissLogDataCount + LogData; % count if no Logger data
+                end
+            end
+        end
+        
     end
 end
-if ishandle(Fid)
-    close(Fid)
-end
+close(Fid)
+close(FidAll)
+fprintf(1,'Total # of experiments: %d\n', AllExpCount)
+fprintf(1, 'Experiments with missing microphone data: %d/%d, %d%%\n', ExpMissMicDataCount,AllExpCount, round(ExpMissMicDataCount*100/AllExpCount))
+fprintf(1, 'Experiments with Mic data that are too short: %d/%d, %d%%\n', ExpTooShortCount, AllExpCount-ExpMissMicDataCount, round(ExpTooShortCount*100/(AllExpCount-ExpMissMicDataCount)))
+fprintf(1, 'Experiments with Mic data, longer that %d min that have too few calls: %d/%d, %d%%\n',MinDur, ExpTooFewCallsCount, AllExpCount-ExpMissMicDataCount-ExpTooShortCount, round(ExpTooFewCallsCount*100/(AllExpCount-ExpMissMicDataCount-ExpTooShortCount)))
+fprintf(1, 'Experiments with Mic data, longer that %d min, with >= %d calls that have no logger data: %d/%d, %d%%\n',MinDur, MinVoc,ExpMissLogDataCount, AllExpCount-ExpMissMicDataCount-ExpTooShortCount-ExpTooFewCallsCount, round(ExpMissLogDataCount*100/(AllExpCount-ExpMissMicDataCount-ExpTooShortCount-ExpTooFewCallsCount)))
+diary OFF
